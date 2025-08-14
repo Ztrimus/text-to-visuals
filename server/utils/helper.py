09643b5d260abcc6bde330843812322fa29b3592
Saurabh Zinjad <Saurabh.Zinjad@phoenix.edu>
2025-08-14 11:12:08 -0700

    FEAT: add measure_execution_time utility

--- server/utils/helper.py
+++ server/utils/helper.py
@@ -7,3 +7,23 @@ Developer Email: saurabhzinjad@gmail.com
 Copyright (c) 2023-2025 Saurabh Zinjad. All rights reserved | https://github.com/Ztrimus
 -----------------------------------------------------------------------
 """
+
+import time
+
+
+def measure_execution_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        func_run_log = (
+            f"Function {func.__name__} took {execution_time:.4f} seconds to execute"
+        )
+        print(func_run_log)
+        # if 'is_st' in kwargs and kwargs['is_st']:
+        #     st.write(func_run_log)
+
+        return result
+
+    return wrapper
